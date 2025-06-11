e021d027eb508c2ede26a240e223a9112d1d7b21
Harnioff <harnioff@gmail.com>
2025-06-11 20:27:10 +0300

    Added Firefox setUp

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
@@ -48,7 +49,9 @@ public class BaseTest {
             driver = new ChromeDriver(chromeOptions);
         }
         if ((browser.equalsIgnoreCase("firefox"))) {
-            driver = new FirefoxDriver();
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+            driver = new FirefoxDriver(options);
         }
 
         context.setAttribute("driver", driver);
